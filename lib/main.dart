fad314bb0c76c1c5d3eb6bb0c4f8f8fb3e321cec
Ben Tigere <munenyashatigere@email.com>
2025-11-14 20:40:53 +0000

    refactor: move app_styles.dart to views directory and update import path

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'app_styles.dart';
 
+import 'package:sandwich_shop/views/app_styles.dart';
 enum BreadType { white, wheat, wholemeal }
 
 void main() {
